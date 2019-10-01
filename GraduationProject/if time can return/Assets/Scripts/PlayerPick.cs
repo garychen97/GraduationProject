325c7888b13548e02a9c464f816068a931f979ea
garychen97 <794767280@qq.com>
2019-10-01 16:36:44 +0800

    为人民服务

---
+++ GraduationProject/if time can return/Assets/Scripts/PlayerPick.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPick : MonoBehaviour
+{
+    public GameObject jiaohu;
+
+    public bool CanTouch = false;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        
+        if (collision.tag == "Player")
+        {
+         
+            jiaohu.SetActive(true);
+            CanTouch = true;
+        }
+       
+        
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+       
+        if (collision.tag == "Player")
+        {
+            jiaohu.SetActive(false);
+            CanTouch = false;
+        }
+    }
+    void OnTouched()
+    {
+        if (CanTouch == true)
+        {
+            Debug.Log("该物体被选中了！");
+
+        }
+        
+    }
+}
---
+++ GraduationProject/if time can return/Assets/Scripts/TouchManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour
+{
+    
+    private float timeHit = 0f;         //用于点击的时间间隔,每次点击时间间隔应大于0.2秒  
+
+    void Update()
+    {
+        timeHit += Time.deltaTime;
+        if (timeHit > 0.2f)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                timeHit = 0f;
+                RaycastHit2D hit;
+               hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+                if (hit)
+                {
+                    hit.collider.gameObject.SendMessage("OnTouched", SendMessageOptions.DontRequireReceiver);
+                    
+                }
+            }
+        }
+    }
+}
